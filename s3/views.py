29ea097da0bd1a0d26f153a13e406e555fb8f3dd
purupuru <keisamurai@gmail.com>
2018-11-23 22:43:57 +0900

    20181123

--- s3/views.py
+++ s3/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
 
+from s3.models import *
 # Create your views here.
+
+class StockListView(TemplateView):
+    template_name = "stock_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(StockListView, self).get_context_data(**kwargs)
+
+        stock = Stock.objects.all() # DBからオブジェクトを取得
+        # workers = Worker.objects.filter(person__sex=Person.MAN)
+        context['stock'] = stock # 別の入れ物に入れる
+
+        return render(self.request, self.template_name, context)
--- s3pj/urls.py
+++ s3pj/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import s3.views as s3_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stock_list/', s3_view.StockListView.as_view()) 
 ]
